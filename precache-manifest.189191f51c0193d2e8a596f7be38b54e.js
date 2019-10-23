dd2df2d22b8aee414b46cd86919cb46ce4d1086e
TaylorAkin <taylorakin.ky@gmail.com>
2019-10-23 11:45:41 -0400

    Updates

---
+++ precache-manifest.189191f51c0193d2e8a596f7be38b54e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ea97bb4ac34caab3a0af470530b6e9",
+    "url": "/react_restaurant/index.html"
+  },
+  {
+    "revision": "3be1888fb98e933c06d7",
+    "url": "/react_restaurant/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4e820751da51254d78ce",
+    "url": "/react_restaurant/static/css/main.f5f21a76.chunk.css"
+  },
+  {
+    "revision": "3be1888fb98e933c06d7",
+    "url": "/react_restaurant/static/js/2.bde28345.chunk.js"
+  },
+  {
+    "revision": "4e820751da51254d78ce",
+    "url": "/react_restaurant/static/js/main.dd035c04.chunk.js"
+  },
+  {
+    "revision": "6f464dfd6f51f10e429f",
+    "url": "/react_restaurant/static/js/runtime-main.ba2e793e.js"
+  }
+]);
